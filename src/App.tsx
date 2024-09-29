93b720311293ddb57c534705037c8ad180455561
Nick Aguila <naguila-mandich@redventures.com>
2024-09-28 21:45:34 -0400

    feat: configs and more setup

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import './App.css';
+import LoginButton from './components/Buttons/LoginButtons';
+import LogoutButton from './components/Buttons/LogoutButton';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const { isAuthenticated, user, getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    const syncUser = async () => {
+      if (isAuthenticated) {
+        const token = await getAccessTokenSilently();
+        await axios.post(
+          'http://localhost:8080/auth/signup',
+          {
+            email: user?.email,
+            name: user?.name,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+      }
+    };
+
+    syncUser();
+  }, [isAuthenticated, getAccessTokenSilently, user]);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      {isAuthenticated ? (
+        <>
+          <p>Welcome, {user?.name}</p>
+          <LogoutButton />
+        </>
+      ) : (
+        <LoginButton />
+      )}
+    </div>
+  );
+}
+
+export default App;
